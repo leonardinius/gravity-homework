4b4966d432bea5d7dd72473b78a05ed7a5ed41e9
Leo <leonidms@gmail.com>
2021-10-16 15:42:53 +0300

    Back to vanilla python. No more flack. Experiment caput

--- app.py
+++ app.py
@@ -1,32 +1,31 @@
+import asyncio
 import os
+from dotenv import load_dotenv
 
+from binance import BinanceSocketManager
 from binance.client import AsyncClient
-from flask import Flask, g
+
+load_dotenv()
 
 API_KEY = os.getenv('BINANCE_API_KEY')
 API_SECRET = os.getenv('BINANCE_API_SECRET')
-print('Binance API Key: %s...%s' % (API_KEY[:3], API_KEY[-3:]))
-
-app = Flask(__name__)
 
 
-async def get_binance_client():
-    if 'binance_client' not in g:
-        g.binance_client = await AsyncClient.create(API_KEY, API_SECRET)
+async def main():
+    print('Binance API Key: %s...%s' % (API_KEY[:3], API_KEY[-3:]))
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client)
+    # start any sockets here, i.e a trade socket
+    ts = bm.trade_socket('BTCUSDT')
+    # then start receiving messages
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
 
-    return g.binance_client
+    await client.close_connection()
 
 
-@app.teardown_appcontext
-async def teardown_binance_client(exception):
-    binance_client = g.pop('binance_client', None)
-
-    if binance_client is not None:
-        await binance_client.close_connection()
-
-
-@app.route("/")
-async def index():
-    client = await get_binance_client()
-    data = await client.get_exchange_info()
-    return data
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
