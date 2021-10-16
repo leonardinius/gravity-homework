04ad9d0826e83fae0b8312532475a214b80ca901
Leo <leonidms@gmail.com>
2021-10-16 14:50:41 +0300

    Em python asyncio story is fucked up

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
+import os
+
+from binance.client import AsyncClient
+from flask import Flask, g
+
+API_KEY = os.getenv('BINANCE_API_KEY')
+API_SECRET = os.getenv('BINANCE_API_SECRET')
+print('Binance API Key: %s...%s' % (API_KEY[:3], API_KEY[-3:]))
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
+async def get_binance_client():
+    if 'binance_client' not in g:
+        g.binance_client = await AsyncClient.create(API_KEY, API_SECRET)
+
+    return g.binance_client
+
+
+@app.teardown_appcontext
+async def teardown_binance_client(exception):
+    binance_client = g.pop('binance_client', None)
+
+    if binance_client is not None:
+        await binance_client.close_connection()
+
+
+@app.route("/")
+async def index():
+    client = await get_binance_client()
+    data = await client.get_exchange_info()
+    return data
